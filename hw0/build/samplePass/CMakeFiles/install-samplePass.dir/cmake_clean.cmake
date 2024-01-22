4dc42c8e3b95f5179acb58475a3d4edbca7867a5
Lai Wang <lai@gatech.edu>
2024-01-21 22:33:40 -0500

    clean up

--- hw0/build/samplePass/CMakeFiles/install-samplePass.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-samplePass"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-samplePass.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- hw0/build/samplePass/CMakeFiles/samplePass.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for samplePass.
-# This may be replaced when dependencies are built.
